e08075015aeba210d4d6bc3570b5708bb0bf129b
Vladislav Mozharov <mozharovlad@gmail.com>
2024-05-29 16:13:17 +0300

    ngrok, paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,29 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
   },
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-};
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import {ngrok} from 'vite-plugin-ngrok'
+import dotenv from 'dotenv'
+
+const envDirectory = `${process.cwd()}/.env`
+const envLocalDirectory = `${process.cwd()}/.env.local`
+dotenv.config({
+  path: [envLocalDirectory, envDirectory],
+  override: false,
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    ngrok({
+      authtoken: process.env.NGROK_TOKEN,
+      domain: process.env.NGROK_DOMAIN,
+    }),
+    react(),
+    tsconfigPaths(),
+  ],
   server: {
     port: 8443,
   },
-});
+  publicDir: './public',
+})
